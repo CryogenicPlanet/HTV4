d957a2261e808e2cc266e2ed592c1724dae9c4df
zackradisic <zack@theradisic.com>
2020-02-15 14:28:41 -0500

    route changes

--- src/routes/fridge/fridge-route.js
+++ src/routes/fridge/fridge-route.js
@@ -4,8 +4,8 @@ import fridgeController from '../../controllers/fridge-controller'
 const router = Router()
 
 router.get('/', fridgeController.get)
-router.get('/food/add/barcode', fridgeController.foodAddFromBarcode)
-router.get('/food/add/', fridgeController.foodAddML)
-router.get('/food/remove', fridgeController.foodRemove)
+router.post('/food/add/barcode', fridgeController.foodAddFromBarcode)
+router.post('/food/add/', fridgeController.foodAddML)
+router.post('/food/remove', fridgeController.foodRemove)
 
 export default router

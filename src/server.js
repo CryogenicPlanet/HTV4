61362d712c9132026967b5f9f280d2cddff97119
zackradisic <zack@theradisic.com>
2020-02-15 22:38:04 -0500

    disable cors

--- src/server.js
+++ src/server.js
@@ -23,6 +23,12 @@ app.use( (err, req, res, next) => {
     return next()
 })
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
 app.get('/api/test', (req, res, next) => {
 	res.status(200).send('success!')
 })
